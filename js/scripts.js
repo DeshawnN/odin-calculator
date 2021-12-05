3b43bb24db56eee2bea1ff7237934d813d96223d
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-12-05 14:44:50 -0500

    feat: add functionality for calculating simple expressions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,48 @@
 const display = document.querySelector(".display");
+let operands = [];
+let operator;
 
-const clearButton = document.querySelector('.clear-button');
+const clearButton = document.querySelector('.calculation-buttons button[data-operation="clear"]');
 clearButton.addEventListener('click', clear);
 
-clear();
+const addButton = document.querySelector(`.calculation-buttons button[data-operation="add"]`);
+addButton.addEventListener('click', function() {
+    operands[0] = +display.textContent;
+    display.textContent = "";
+    operator = add;
+});
+
+const subtractButton = document.querySelector('.calculation-buttons button[data-operation="subtract"]');
+subtractButton.addEventListener('click', function() {
+    operands[0] = +display.textContent;
+    display.textContent = "";
+    operator = subtract;
+});
+
+const multiplyButton = document.querySelector('.calculation-buttons button[data-operation="multiply"]');
+multiplyButton.addEventListener('click', () => {
+    operands[0] = +display.textContent;
+    display.textContent = "";
+    operator = multiply;
+});
+
+const divideButton = document.querySelector('.calculation-buttons button[data-operation="divide"]');
+divideButton.addEventListener('click', () => {
+    operands[0] = +display.textContent;
+    display.textContent = "";
+    operator = divide;
+});
+
+const equalButton = document.querySelector('.calculation-buttons button[data-operation="operate"]');
+equalButton.addEventListener('click', function() {
+    operands[1] = +display.textContent;
+    let result = operator(operands[0], operands[1]);
+    if (Math.floor(result) !== result) {
+        result = result.toFixed(2);
+    }
+    operands[0] = result;
+    display.textContent = result;
+});
 
 const buttons = document.querySelectorAll(".number-buttons button");
 buttons.forEach(button => button.addEventListener('click', event => {
@@ -26,10 +65,15 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
-    return operator(a, b);
+function operate(action, a, b) {
+    const result = action(a, b);
+    operands = [result];
+    operator = null;
+    return result;
 }
 
 function clear() {
+    operands = [];
+    operator = null;
     display.textContent = '';
 }
