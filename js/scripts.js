a965398b9fcdc725a80264224263fb15f2422ecd
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-12-04 21:40:39 -0500

    feat: add clear button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+const clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener('click', clear);
+
 function add(a, b) {
     return a + b;
 }
@@ -16,4 +19,9 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
     return operator(a, b);
+}
+
+function clear() {
+    const display = document.querySelector(".display");
+    display.textContent = '';
 }
